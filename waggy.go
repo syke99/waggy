27145cd6089218d820833657ad465ed4a1f886df
syke99 <quinnmillican@gmail.com>
2022-11-27 19:01:17 -0600

    added Init() method to supply WaggyRequestWriter and WaggyRequest pointers

---
+++ waggy.go
@@ -0,0 +1,11 @@
+package waggy
+
+// Init initializes the request and provides a *WaggyResponseWriter
+// to use for writing responses, and a *WaggyRequest to use for
+// retrieving info about the incoming HTTP request
+func Init() (*WaggyResponseWriter, *WaggyRequest) {
+	w := Response()
+	r := Request()
+
+	return w, r
+}
