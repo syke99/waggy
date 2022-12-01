0169aa36c6b010d33e90b7f45151002ba793ecf2
syke99 <quinnmillican@gmail.com>
2022-12-01 08:18:24 -0600

    correct printing with default responses

--- v2/waggy.go
+++ v2/waggy.go
@@ -1,7 +1,7 @@
 package v2
 
 import (
-	"errors"
+	"fmt"
 	"net/http"
 	"reflect"
 
@@ -13,30 +13,35 @@ import (
 // default response inside the handler it is being used inside of.
 // If no default response has been set, this function will return
 // an error.
-func WriteDefaultResponse(w http.ResponseWriter, r *http.Request) (int, error) {
+func WriteDefaultResponse(w http.ResponseWriter, r *http.Request) {
 	rv := r.Context().Value(resources.DefResp)
 	if rv == nil {
-		return 0, resources.NoDefaultResponse
+		fmt.Fprintln(w, resources.NoDefaultResponse.Error())
 	}
 
 	results := reflect.ValueOf(rv).Call([]reflect.Value{reflect.ValueOf(w)})
 
-	return int(results[0].Int()), errors.New(results[1].String())
+	if len(results) == 2 &&
+		results[1].Interface().(error) != nil {
+		fmt.Fprintln(w, results[1].Interface().(error).Error())
+	}
 }
 
 // WriteDefaultErrorResponse returns the result of writing the set
 // default error response inside the handler it is being used inside of.
 // If no default error response has been set, this function will return
 // an error.
-func WriteDefaultErrorResponse(w http.ResponseWriter, r *http.Request) error {
+func WriteDefaultErrorResponse(w http.ResponseWriter, r *http.Request) {
 	rv := r.Context().Value(resources.DefErr)
 	if rv == nil {
-		return resources.NoDefaultErrorResponse
+		fmt.Fprintln(w, resources.NoDefaultErrorResponse.Error())
 	}
 
 	results := reflect.ValueOf(rv).Call([]reflect.Value{reflect.ValueOf(w)})
 
-	return results[0].Interface().(error)
+	if len(results) != 0 {
+		fmt.Fprintln(w, results[0].Interface().(error).Error())
+	}
 }
 
 // Vars returns the route variables for the current request, if any.
