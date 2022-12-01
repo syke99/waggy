354dc0ebbac06ecb20443297cfc5bc479a4d3bf3
syke99 <quinnmillican@gmail.com>
2022-12-01 08:11:03 -0600

    more unit testing

--- v2/handler_test.go
+++ v2/handler_test.go
@@ -111,7 +111,7 @@ func TestWaggyHandler_MethodHandler(t *testing.T) {
 	}
 }
 
-func TestWaggyHandler_ServeHTTP(t *testing.T) {
+func TestWaggyHandler_ServeHTTP_MethodGet(t *testing.T) {
 	// Arrange
 	os.Setenv(resources.XMatchedRoute.String(), resources.TestRoute)
 	os.Setenv(resources.RequestMethod.String(), http.MethodGet)
@@ -168,4 +168,63 @@ func TestWaggyHandler_ServeHTTP(t *testing.T) {
 	}
 
 	assert.Equal(t, fmt.Sprintf("%s\n", resources.Hello), wr.Body.String())
+}
+
+func TestWaggyHandler_ServeHTTP_MethodDelete(t *testing.T) {
+	// Arrange
+	os.Setenv(resources.XMatchedRoute.String(), resources.TestRoute)
+	os.Setenv(resources.RequestMethod.String(), http.MethodDelete)
+
+	helloHandler := func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, resources.Hello)
+	}
+	goodbyeHandler := func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, resources.Goodbye)
+	}
+
+	w := InitHandlerWithRoute(resources.TestRoute)
+
+	w.MethodHandler(http.MethodGet, helloHandler)
+	w.MethodHandler(http.MethodDelete, goodbyeHandler)
+
+	w.WithDefaultResponse([]byte(resources.HelloWorld))
+
+	testErr := WaggyError{
+		Type:   resources.TestRoute,
+		Title:  "",
+		Detail: resources.TestError.Error(),
+		Status: 0,
+	}
+
+	w.WithDefaultErrorResponse(testErr, http.StatusInternalServerError)
+
+	r, _ := http.NewRequest(http.MethodGet, resources.TestRoute, nil)
+
+	wr := httptest.NewRecorder()
+
+	// Act
+	w.ServeHTTP(wr, r)
+
+	// Assert
+	assert.IsType(t, &WaggyHandler{}, w)
+	assert.Equal(t, resources.TestRoute, w.route)
+	assert.IsType(t, []byte{}, w.defResp)
+	assert.Equal(t, len(resources.HelloWorld), len(w.defResp))
+	assert.Equal(t, resources.HelloWorld, string(w.defResp))
+	assert.Equal(t, testErr, w.defErrResp)
+	assert.Equal(t, resources.TestRoute, w.defErrResp.Type)
+	assert.Equal(t, resources.TestError.Error(), w.defErrResp.Detail)
+	assert.Equal(t, http.StatusInternalServerError, w.defErrRespCode)
+	assert.IsType(t, map[string]http.HandlerFunc{}, w.handlerMap)
+
+	for k, v := range w.handlerMap {
+		switch k {
+		case http.MethodGet:
+			assert.Equal(t, resources.GetFunctionName(helloHandler), resources.GetFunctionName(v))
+		case http.MethodDelete:
+			assert.Equal(t, resources.GetFunctionName(goodbyeHandler), resources.GetFunctionName(v))
+		}
+	}
+
+	assert.Equal(t, fmt.Sprintf("%s\n", resources.Goodbye), wr.Body.String())
 }
--- v2/router_test.go
+++ v2/router_test.go
@@ -1,1 +1,95 @@
 package v2
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"github.com/syke99/waggy/v2/internal/resources"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+)
+
+func TestInitRouter(t *testing.T) {
+	// Act
+	w := InitRouter()
+
+	// Assert
+	assert.IsType(t, &WaggyRouter{}, w)
+	assert.IsType(t, map[string]*WaggyHandler{}, w.router)
+	assert.Equal(t, 0, len(w.router))
+}
+
+func TestWaggyRouter_Handle(t *testing.T) {
+	// Arrange
+	w := InitRouter()
+
+	helloHandler := InitHandler()
+	goodbyeHandler := InitHandler()
+
+	// Act
+	w.Handle("/hello", helloHandler)
+	w.Handle("/goodbye", goodbyeHandler)
+
+	// Assert
+	assert.IsType(t, &WaggyHandler{}, w.router["/hello"])
+	assert.IsType(t, &WaggyHandler{}, w.router["/goodbye"])
+}
+
+func TestWaggyRouter_ServeHTTP_MethodGet(t *testing.T) {
+	// Arrange
+	wr := InitRouter()
+
+	os.Setenv(resources.XMatchedRoute.String(), resources.TestRoute)
+	os.Setenv(resources.RequestMethod.String(), http.MethodGet)
+
+	helloHandler := func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, resources.Hello)
+	}
+
+	wh := InitHandler().
+		MethodHandler(http.MethodGet, helloHandler)
+
+	wr.Handle(resources.TestRoute, wh)
+
+	r, _ := http.NewRequest(http.MethodGet, resources.TestRoute, nil)
+
+	rr := httptest.NewRecorder()
+
+	// Act
+	wr.ServeHTTP(rr, r)
+
+	// Assert
+	assert.IsType(t, &WaggyRouter{}, wr)
+	assert.IsType(t, &WaggyHandler{}, wr.router[resources.TestRoute])
+	assert.Equal(t, fmt.Sprintf("%s\n", resources.Hello), rr.Body.String())
+}
+
+func TestWaggyRouter_ServeHTTP_MethodDelete(t *testing.T) {
+	// Arrange
+	wr := InitRouter()
+
+	os.Setenv(resources.XMatchedRoute.String(), resources.TestRoute)
+	os.Setenv(resources.RequestMethod.String(), http.MethodDelete)
+
+	goodbyeHandler := func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, resources.Hello)
+	}
+
+	wh := InitHandler().
+		MethodHandler(http.MethodDelete, goodbyeHandler)
+
+	wr.Handle(resources.TestRoute, wh)
+
+	r, _ := http.NewRequest(http.MethodGet, resources.TestRoute, nil)
+
+	rr := httptest.NewRecorder()
+
+	// Act
+	wr.ServeHTTP(rr, r)
+
+	// Assert
+	assert.IsType(t, &WaggyRouter{}, wr)
+	assert.IsType(t, &WaggyHandler{}, wr.router[resources.TestRoute])
+	assert.Equal(t, fmt.Sprintf("%s\n", resources.Goodbye), rr.Body.String())
+}
